d535b39de03ad006ef264b2c11f72351beade228
Марк Емельянченко <mav.emelianchenko@stc-spb.ru>
2024-01-20 14:36:25 +0300

    Tests were added. Project structure was altered

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-#include <gtest/gtest.h>
 
-
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
-
-int main(int argc, char* argv[])
+int main()
 {
-    // std::cout << "Hello World" << std::endl;
-    // return 0;
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    std::cout << "Hello World" << std::endl;
+    return 0;
 }
 
 
