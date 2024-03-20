06ce5ed0e2aa62eb1ff4273fb4b1d29f677b03cb
Марк Емельянченко <mav.emelianchenko@stc-spb.ru>
2024-03-20 12:02:36 +0300

    loop has been modified

--- libs/spacebattle/include/loop.h
+++ libs/spacebattle/include/loop.h
@@ -1,17 +1,20 @@
 #ifndef LOOP_H
 #define LOOP_H
 
-#include <commands/command.h>
 #include "threadsafe_q.h"
+#include <commands/command.h>
 #include <condition_variable>
+#include <functional>
 #include <thread>
 
 namespace engine
 {
     class loop
     {
+        using action = std::function<void()>;
+
     public:
-        loop(threadsafe_q<command_shared>& q);
+        loop(threadsafe_q<command_shared> &q);
 
         ~loop();
 
@@ -19,25 +22,55 @@ namespace engine
 
         void stop();
 
+        void set_behavior(const action &&behavior);
+
+        inline bool get_stop_flag() const {return stopFlag;}
+
     private:
         void loop_processor();
         bool stopFlag;
         std::condition_variable cv;
         mutable std::mutex mtx;
         std::thread thr;
-        engine::threadsafe_q<command_shared>& q;
+        engine::threadsafe_q<command_shared> &q;
+        action behavior;
 
-        class command_stop : public command
+    public:
+        class hard_stop : public command
         {
         public:
-            command_stop(loop& loop):_loop(loop){}
-            void execute() override {
-                _loop.stopFlag = true;
+            explicit hard_stop(
+                loop &loop, action &&callback = [] {})
+                : _loop(loop), callback(callback)
+            {
             }
+            inline void execute() override
+            {
+                _loop.stopFlag = true;
+                callback();
+            }
+
         private:
-            loop& _loop;
+            loop &_loop;
+            action callback;
         };
+
+        class soft_stop : public command
+        {
+        public:
+            explicit soft_stop(
+                loop &loop, action &&callback = [] {})
+                : _loop(loop), callback(callback)
+            {
+            }
+
+            void execute() override;
+
+        private:
+            loop &_loop;
+            action callback;
     };
+};
 } // namespace engine
 
 #endif // LOOP_H
